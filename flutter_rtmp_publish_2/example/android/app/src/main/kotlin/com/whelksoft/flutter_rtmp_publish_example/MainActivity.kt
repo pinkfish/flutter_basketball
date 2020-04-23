2aabfefe5155f99bee59f20dca5c64013703dba4
David Bennett <pinkfishfrog@gmail.com>
2020-04-23 16:26:24 -0700

    Remove and unneeded stuff.

--- flutter_rtmp_publish_2/example/android/app/src/main/kotlin/com/whelksoft/flutter_rtmp_publish_example/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.whelksoft.flutter_rtmp_publish_example
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- flutter_rtmp_publish_2/ios/Classes/FlutterRtmpPublishPlugin.h
+++
@@ -1,4 +0,0 @@
-#import <Flutter/Flutter.h>
-
-@interface FlutterRtmpPublishPlugin : NSObject<FlutterPlugin>
-@end
