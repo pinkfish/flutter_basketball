139357ba5d080e12fcbdcd9dc9077f06338132cc
David Bennett <pinkfishfrog@gmail.com>
2020-08-21 10:02:20 -0700

    Update to be Invites.

--- firebase/functions/src/db/invite/onWrite.f.ts
+++ firebase/functions/src/db/invite/onWrite.f.ts
@@ -2,7 +2,7 @@ import * as functions from "firebase-functions";
 import onCreate from "./onCreate.f";
 
 export default functions.firestore
-  .document("Games/{gameUid}")
+  .document("Invites/{gameUid}")
   .onWrite(async change => {
     return onCreate(change.after);
   });
